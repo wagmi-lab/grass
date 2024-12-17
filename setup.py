2c1e00103372031c92948bdab13624541906c0f9
samsonwyt <samsonwyt@hotmail.com>
2024-12-17 16:33:25 +0800

    custom

---
+++ setup.py
@@ -0,0 +1,26 @@
+import pathlib
+from setuptools import find_packages, setup
+
+with open('requirements.txt', 'r') as f:
+    install_reqs = [
+        s for s in [
+            line.split('#', 1)[0].strip(' \t\n') for line in f
+        ] if s != ''
+    ]
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# This call to setup() does all the work
+setup(
+    name='grass-fork',
+    version='0.0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=install_reqs,
+    author='Quant',
+    classifiers=[],
+    entry_points={
+        "console_scripts": []  
+    }
+)
